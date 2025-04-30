d2fba26346625abd888ae64b4fef2d8ec4dc26ad
Ankit Kumar <ankitkumar@Ankits-MacBook-Pro-3.local>
2025-05-01 00:45:30 +0530

    Fix: Install react-slick and correct imports

--- src/components/Home/Featured/Featured.jsx
+++ src/components/Home/Featured/Featured.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css'; 
-
-import Slider from "react-slick"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import FetureCard from "./FeatureCard";
 
 const Featured =()=>{
