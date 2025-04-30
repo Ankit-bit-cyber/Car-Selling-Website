2a08b8e85770baee3a2320b7c33cf8f3820f1b47
Ankit Kumar <ankitkumar@Ankits-MacBook-Pro-3.local>
2025-05-01 00:53:49 +0530

    Fix: Update image paths and move to public folder

--- src/components/Cars/OurCars.jsx
+++ src/components/Cars/OurCars.jsx
@@ -2,44 +2,45 @@ import React from "react";
 import CarsCard from "./CarsCard";
 
 const OurCars =()=>{
-    const carsData =[
+    const carsData = [
         {
-            id: 0,
-            img: "src/assets/img/car15.jpg",
-            name: "Mercedes",
-            price: "60,0000",
+           id: 0,
+           img: "/assets/img/car15.jpg", // Notice the leading "/"
+           name: "Mercedes",
+           price: "60,0000",
         },
         {
-            id: 1,
-            img: "src/assets/img/car6.jpg",
-            name: "Audi",
-            price: "70,0000",
+           id: 1,
+           img: "/assets/img/car6.jpg",
+           name: "Audi",
+           price: "70,0000",
         },
         {
-            id: 2,
-            img: "src/assets/img/car7.jpg",
-            name: "BMW",
-            price: "75,0000",
+           id: 2,
+           img: "/assets/img/car7.jpg",
+           name: "BMW Series-5",
+           price: "75,0000",
         },
         {
-            id: 3,
-            img: "src/assets/img/car8.jpg",
-            name: "Range Rover",
-            price: "50,0000",
+           id: 3,
+           img: "/assets/img/car8.jpg",
+           name: "Range Rover",
+           price: "50,0000",
         },
         {
-            id: 4,
-            img: "src/assets/img/car4.jpg",
-            name: "BMW Series-3",
-            price: "60,00000",
+           id: 4,
+           img: "/assets/img/car4.jpg",
+           name: "BMW Series-3",
+           price: "60,0000",
         },
         {
-            id: 5,
-            img: "src/assets/img/car11.jpg",
-            name: "Cadillac",
-            price: "55,0000",
+           id: 5,
+           img: "/assets/img/car11.jpg",
+           name: "Cadillac",
+           price: "55,0000",
         },
-    ]
+     ]
+     
     return(
         <div className="container pt-24">
             <div>
